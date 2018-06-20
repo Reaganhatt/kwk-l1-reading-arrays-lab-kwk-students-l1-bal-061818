211844b66fb8d6a54629e0be35e097a25d08e52e
Reaganhatt <reagan.hatt@gmail.com>
2018-06-20 15:56:15 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -8,16 +8,16 @@ STUDENT_NAMES = [
 ]
 
 def first_student_by_index
-  puts STUDENT_NAMES[0]
+  return STUDENT_NAMES[0]
 end
 
 
 def fourth_student_by_index
-  puts STUDENT_NAMES[3]
+  return STUDENT_NAMES[3]
 end
 
 def last_student_by_index
-  puts STUDENT_NAMES[5]
+  return STUDENT_NAMES[5]
 end
 
 def first_student_by_method
